65566a7099198ea9a669232bef1aa0c99ab09b47
Arunkumar M <arunofficial669@gmail.com>
2025-08-20 18:15:04 +0530

    Modified

--- src/main/java/com/java/pages/LoginPage.java
+++ src/main/java/com/java/pages/LoginPage.java
@@ -37,6 +37,7 @@ public class LoginPage {
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module")));
         if (successMsg.isDisplayed()) {
             System.out.println("Login Successful!");
+            System.out.println("Login Successful!");
         }
 
         BaseTest base = new BaseTest();
